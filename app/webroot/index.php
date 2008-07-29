b7b5e87b3da16fecdfadd87a9df13352d783cc33
jeremib <jeremib@30fd41d8-f852-0410-ae21-55fca4e2b599>
2008-07-29 03:55:14 +0000

    Starting to add some defaults

--- app/webroot/index.php
+++ app/webroot/index.php
@@ -1,4 +1,22 @@
 <?
+/**
+ * @author Jeremi Bergman <jeremi@innerserve.com>
+ * @package spmvc
+ * @subpackage core
+ *
+ * @copyright Copyright 2008 Jeremi Bergman Licensed under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except in compliance with the
+ * License.
+ *
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language governing permissions
+ * and limitations under the License.
+ */
+
+// defines below borrowed from cakephp
 
 if (!defined('DS')) {
 	define('DS', DIRECTORY_SEPARATOR);
@@ -18,14 +36,15 @@ if (!defined('WWW_ROOT')) {
 	define('WWW_ROOT', dirname(__FILE__) . DS);
 }
 
-if (!defined('CORE_PATH')) {
-	if (function_exists('ini_set') && ini_set('include_path', ROOT . PATH_SEPARATOR . ROOT . DS . APP_DIR . DS . PATH_SEPARATOR . ini_get('include_path'))) {
-		define('APP_PATH', null);
-		define('CORE_PATH', null);
-	} else {
-		define('APP_PATH', ROOT . DS . APP_DIR . DS);
-		define('CORE_PATH', ROOT . DS);
-	}
+
+if (function_exists('ini_set') && ini_set('include_path', ROOT . PATH_SEPARATOR . ROOT . DS . APP_DIR . DS . PATH_SEPARATOR . ini_get('include_path'))) {
+	define('APP_PATH', null);
+	define('CORE_PATH', null);
+} else {
+	define('APP_PATH', ROOT . DS . APP_DIR . DS);
+	define('CORE_PATH', ROOT . DS);
 }
-require ROOT . DS . 'dispatch.php';
+
+// call our dispatcherer
+require 'dispatch.php';
 ?>
