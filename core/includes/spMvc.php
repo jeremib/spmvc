019dd002707439b3584d9f8fb7717bfb29570c31
jeremib <jeremib@30fd41d8-f852-0410-ae21-55fca4e2b599>
2008-07-29 02:29:08 +0000

    Added phpdoc and license info

--- core/includes/spMvc.php
+++ core/includes/spMvc.php
@@ -1,4 +1,21 @@
 <?
+/**
+ * @author Jeremi Bergman <jeremi@innerserve.com>
+ * @package spmvc
+ * @subpackage core
+ * 
+ * @copyright Copyright 2008 Jeremi Bergman Licensed under the Apache License, 
+ * Version 2.0 (the "License"); you may not use this file except in compliance with the 
+ * License. 
+ *  
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0 
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under 
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language governing permissions 
+ * and limitations under the License. 
+ */
+
 class spmvc {
 	private $view;
 	private $model;
@@ -38,6 +55,7 @@ class spmvc {
 		$method_obj->invokeArgs($controller_obj, $parameters);
 		
 		// fetch the view, stick it in the layout
+		// @todo add check for valid view
 		$content_for_layout = $this->view->fetch('app/views/' . $controller . '/' . $action . '.phtml');
 		$this->view->assign('content_for_layout', ( isset($content_for_layout) ? $content_for_layout : '') );
 		$this->view->display('app/views/layouts/default.phtml');
